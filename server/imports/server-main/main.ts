94849084ab28965105ec6c2622fe93c02cf3e427
Nicholas Tsaoucis <rusty.nt@gmail.com>
2017-01-10 11:04:58 +1100

    added onusercreate to set roles per user and to provide defaults when not set.

--- server/imports/server-main/main.ts
+++ server/imports/server-main/main.ts
@@ -2,12 +2,29 @@ import { Roles } from "meteor/alanning:roles";
 
 export class Main {
   start(): void {
+    Accounts.onCreateUser((options, user) => {
+      console.log("new user");
+      console.log(user);
+      console.log(options);
+
+      user.profile = options.profile;
+      user.roles = options.roles || ["spectator"];
+      console.log(user);
+      
+      Roles.addUsersToRoles(user._id, user.roles, Roles.GLOBAL_GROUP);
+
+      return user;
+    });
+
     this.createAdmin();
   }
 
   createAdmin(): void {
     let admin = {
-      name: "admin",
+      profile: {
+        name: "admin"
+      },
+      password: "123456",
       email: "admin@email.com",
       roles: ["admin"]
     };
@@ -15,16 +32,10 @@ export class Main {
     let bob: any = Accounts.findUserByEmail(admin.email);
     // console.log(bob);
     if (!bob) {
-      Meteor.users.remove(bob._id);
-
-      let id = Accounts.createUser({
-        email: admin.email,
-        password: "123456",
-        profile: {
-          name: admin.name
-        }
-      });
-      Roles.addUsersToRoles(id, admin.roles, Roles.GLOBAL_GROUP);
+      // Meteor.users.remove(bob._id);
+      let id = Accounts.createUser(admin);
     }
   }
+
+
 }
